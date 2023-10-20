4e5a1ba5836ced16e13801739cbd5d90b3a98683
recozzi <rebecca.cozzi@outlook.com>
2023-10-20 16:26:17 +0200

    rm bonus

--- push_swap/Libft/libft.h
+++ push_swap/Libft/libft.h
@@ -102,22 +102,4 @@ int			ft_printf(const char *str, ...);
 
 char		*get_next_line(int fd);
 
-// lst =========================================================================== //
-
-typedef struct s_list
-{
-	void			*content;
-	struct s_list	*next;
-}	t_list;
-
-t_list		*ft_lstnew(void *content);
-t_list		*ft_lstlast(t_list *lst);
-t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
-void		ft_lstadd_back(t_list **lst, t_list *new);
-void		ft_lstadd_front(t_list **lst, t_list *new);
-void		ft_lstclear(t_list **lst, void (*del)(void*));
-void		ft_lstdelone(t_list *lst, void (*del)(void*));
-void		ft_lstiter(t_list *lst, void (*f)(void *));
-int			ft_lstsize(t_list *lst);
-
 #endif
